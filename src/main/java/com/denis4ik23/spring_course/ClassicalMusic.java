b2080987a480ace79791a8d2d8d6dc1032d64d7a
denis4ik23 <denis4ik2387@gmail.com>
2022-12-04 01:35:27 +0300

    PostConstruct PreDestroy

--- src/main/java/com/denis4ik23/spring_course/ClassicalMusic.java
+++ src/main/java/com/denis4ik23/spring_course/ClassicalMusic.java
@@ -3,9 +3,22 @@ package com.denis4ik23.spring_course;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-@Scope("singleton")
 public class ClassicalMusic implements Music {
+    //java 9 deprecated, java 11 deleted.
+    // И теперь для их использования надо добавлять javax.annotation-api зависимость(pom.xml).
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
 
     @Override
     public String getSong() {
