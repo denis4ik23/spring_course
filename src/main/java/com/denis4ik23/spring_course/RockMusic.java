bce3e234126d5082e407c6c85cc7dc665c70f9be
denis4ik23 <denis4ik2387@gmail.com>
2022-12-04 00:58:47 +0300

    Scope prototype

--- src/main/java/com/denis4ik23/spring_course/RockMusic.java
+++ src/main/java/com/denis4ik23/spring_course/RockMusic.java
@@ -1,8 +1,10 @@
 package com.denis4ik23.spring_course;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class RockMusic implements Music {
     @Override
     public String getSong() {
