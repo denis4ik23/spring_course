e27307b1fa25f01d1395f618bc10e480e87066ce
denis4ik23 <denis4ik2387@gmail.com>
2022-12-03 23:04:01 +0300

    Qualifier field

--- src/main/java/com/denis4ik23/spring_course/MusicPlayer.java
+++ src/main/java/com/denis4ik23/spring_course/MusicPlayer.java
@@ -1,21 +1,17 @@
 package com.denis4ik23.spring_course;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
-
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
-    }
+    @Qualifier("classicalMusic")
+    private Music music;
 
     public String playMusic() {
-        return "Playing: " + classicalMusic.getSong();
+        return "Playing: " + music.getSong();
 
     }
 }
