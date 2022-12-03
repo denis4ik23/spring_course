ac8a43cf891dacddf313a343104cf71fc5178529
denis4ik23 <denis4ik2387@gmail.com>
2022-12-04 00:48:00 +0300

    Scope singleton

--- src/main/java/com/denis4ik23/spring_course/ClassicalMusic.java
+++ src/main/java/com/denis4ik23/spring_course/ClassicalMusic.java
@@ -1,8 +1,10 @@
 package com.denis4ik23.spring_course;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("singleton")
 public class ClassicalMusic implements Music {
 
     @Override
